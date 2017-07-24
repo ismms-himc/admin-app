edaeb17eb9cac32bd133fb03d35d187615e7c932
cornhundred <nickfloresfernandez@gmail.com>
2017-07-24 12:49:41 -0400

    edit button showing up, form not showing

--- test-admin/src/posts.js
+++ test-admin/src/posts.js
@@ -21,17 +21,28 @@
 // a user. Admin-on-REST knows how to take advantage of these foreign keys to
 // fetch references. For instance, to include the user name in the post list,
 // use the <ReferenceField>:
+//
+// While displaying the posts list, the app now fetches user records, and
+// displays their name as a <TextField>. Notice the label property: you can use
+// it on any field component to customize the field label.
+// Tip: Reference components always pass the data they fetch to a child
+// component, which is responsible for displaying the data.
+//
+// Creation and Editing
+// An admin interface is about displaying remote data, but also about editing
+// and creating. Admin-on-REST provides <Create> and <Edit> components for that
+// purpose. Add them to the posts script:
 
 // in src/posts.js
 import React from 'react';
-import { List, Datagrid, TextField, EmailField, ReferenceField } from 'admin-on-rest';
+import { List, Edit, Create, Datagrid, ReferenceField, TextField, EditButton, DisabledInput, LongTextInput, ReferenceInput, SelectInput, SimpleForm, TextInput } from 'admin-on-rest';
 
 // export const PostList = (props) => (
 export const PostList = function(props) {
 
-  console.log(props);
+  {/*console.log(props);*/}
 
-  return (<List title='Something' {...props}>
+  return (<List title='Post Board' {...props}>
     <Datagrid>
       <TextField source='id' />
       {/* getting user's name from users using userId from posts */}
@@ -39,8 +50,38 @@ export const PostList = function(props) {
         <TextField source='name' />
       </ReferenceField>
       <TextField source='title' />
-      <TextField source='body' />
+      <TextField label='Post Text' source='body' />
+      <EditButton />
     </Datagrid>
   </List>);
 }
 // );
+
+export const PostTitle = ({ record }) => {
+  return <span>Post {record ? `"${record.title}"` : ''}</span>;
+};
+
+export const PostEdit = (props) => (
+  <Edit title={<PostTitle />} {...props}>
+    <SimpleForm>
+      <DisabledInput source='id' />
+      <ReferenceInput label='User' source='userId' reference='users'>
+        <SelectInput optionText='name' />
+      </ReferenceInput>
+      <TextInput source='title' />
+      <LongTextInput source='body' />
+    </SimpleForm>
+  </Edit>
+)
+
+export const PostCreate = (props) => (
+  <Create {...props}>
+    <SimpleForm>
+      <ReferenceInput label='User' source='userId' reference='users' allowEmpty>
+        <SelectInput optionText='name' />
+      </ReferenceInput>
+      <TextInput source='title' />
+      <LongTextInput source='body' />
+    </SimpleForm>
+  </Create>
+);
